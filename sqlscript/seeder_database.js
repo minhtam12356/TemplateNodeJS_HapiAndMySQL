8d3f0b557f518664cf118d7095a3abd01cfa5d3c
chaunnt <chaupad@gmail.com>
2021-08-01 10:34:49 +0700

    update template

--- sqlscript/seeder_database.js
+++ sqlscript/seeder_database.js
@@ -1,16 +1,4 @@
-const Staff = require('../Staff/resourceAccess/StaffResourceAccess');
-// const ETHWallet = require('../ETHWallet/resourceAccess/ETHWalletResourceAccess');
-const BetRecords = require('../BetRecords/resourceAccess/BetRecordsResourceAccess');
-const DepositTransaction = require('../DepositTransaction/resourceAccess/DepositTransactionResourceAccess');
-const Role = require('../Role/resourceAccess/RoleResourceAccess');
 const User = require('../User/resourceAccess/UserResourceAccess');
-const UserCommision = require('../Commission/resourceAccess/UserCommisionResourceAccess');
-const Wallet = require('../Wallet/resourceAccess/WalletResourceAccess');
-const WithdrawTransaction = require('../WithdrawTransaction/resourceAccess/WithdrawTransactionResourceAccess');
-const ExchangeTransaction = require('../ExchangeTransaction/resourceAccess/ExchangeTransactionResourceAccess');
-const {DEPOSIT_TRX_STATUS} = require('../DepositTransaction/DepositTransactionConstant');
-const {WITHDRAW_TRX_STATUS} = require('../WithdrawTransaction/WithdrawTransactionConstant');
-const {EXCHANGE_TRX_STATUS} = require('../ExchangeTransaction/ExchangeTransactionConstant');
 
 async function seedDatabase() {
   console.log("seedDatabase");
