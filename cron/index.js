f25ef5f72ec4aca418bccc8ace6da067fa01ecef
Chau Nguyen <chau.nguyen@teledyneflir.com>
2021-12-11 10:53:23 +0700

    add sample cronjob

--- cron/index.js
+++ cron/index.js
@@ -5,6 +5,8 @@
 
 const { CronInstance, executeJob } = require("../ThirdParty/Cronjob/CronInstance");
 const Logger = require('../utils/logging');
+
+const CustomerRecordJob = require('../API/CustomerRecord/cronjob/StationsRecordAutoCheck');
 const CustomerMessageJob = require('../API/CustomerMessage/cronjob/StationsMessageAutoSend');
 
 async function startSchedule() {
@@ -18,11 +20,13 @@ async function startSchedule() {
   //every 30 seconds
   setInterval(CustomerMessageJob.autoSendMessageForCustomer, 30 * 1000);
 
-  //every 1 minutes
-  CronInstance.schedule('* * * * *', async function () {
-    executeJob('./cron/crawlerChapterDetail.js');
-  });
+  //every 30 seconds
+  setInterval(CustomerRecordJob.autoUpdateProcessForStation, 30 * 1000);
 
+  // every day - at the end of day
+  CronInstance.schedule('0 23 * * *', async function () {
+    executeJob('./API/CustomerRecord/cronjob/StationsRecordCompleteJob.js');
+  });
 }
 
 module.exports = {
